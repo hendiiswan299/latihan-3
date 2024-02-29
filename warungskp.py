be48ae2a74b9321d0e4ff1a00a8e83d6fab22f77
hendiiswan299 <160814751+hendiiswan299@users.noreply.github.com>
2024-02-29 11:25:49 +0700

    Add files via upload

---
+++ warungskp.py
@@ -0,0 +1,9 @@
+#ucup membeli barang ke warung
+rokok = 20000
+minyak = 25000
+gula = 11000
+saldo = 100000
+diskon = 10000
+#hitung jumlah kembalian ucup
+Kembalian=saldo-(rokok+minyak+gula-diskon)
+print("Kembalian ucup = ",Kembalian)
